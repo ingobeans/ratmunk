26acceba79ffec2cb31b0b6192cfddd2b152eb6f
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-09-27 16:02:10 +0200

    set up basic auto-tiling

--- healthbar.gd
+++ healthbar.gd
@@ -1,6 +1,6 @@
 extends ColorRect
 
-@onready var player = self.get_parent()
+@onready var player = self.get_parent().get_parent()
 
 func _process(_delta: float):
 	size.x = player.health / 100.0 * 72.0
